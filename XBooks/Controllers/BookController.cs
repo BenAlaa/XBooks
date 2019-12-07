f092e2ed269f032b697dae493cb8e43d85975ee6
BenAlaa <46384841+BenAlaa@users.noreply.github.com>
2019-12-07 18:48:43 +0200

    Add Delet Book Api to Book Api Controller

--- XBooks/Controllers/BookController.cs
+++ XBooks/Controllers/BookController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using XBooks.Models;
 
 namespace XBooks.Controllers
@@ -18,9 +19,24 @@ namespace XBooks.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetAll()
+        public async Task<IActionResult> GetAll()
         {
-            return Json(new { data = _db.Book.ToList() }); 
+            return Json(new { data = await _db.Book.ToListAsync() }); 
         }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete (int id)
+        {
+            var book = await _db.Book.FirstOrDefaultAsync(b => b.Id == id);
+            if (book == null)
+            {
+                return Json(new { success = false, message = " Error While Deleting" });
+            }
+            _db.Book.Remove(book);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = " Delete Succesful" });
+
+        }
+
     }
 }
