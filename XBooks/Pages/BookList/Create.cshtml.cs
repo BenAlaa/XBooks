db6e55a0f2a0dced36c0186608fdf93f43ec69d0
BenAlaa <46384841+BenAlaa@users.noreply.github.com>
2019-12-07 09:40:46 +0200

    Handle create book form OnPost Method

--- XBooks/Pages/BookList/Create.cshtml.cs
+++ XBooks/Pages/BookList/Create.cshtml.cs
@@ -16,10 +16,25 @@ namespace XBooks
             _db = db;
         }
 
+        [BindProperty]
         public Book Book { get; set; }
         public void OnGet()
         {
 
         }
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                await _db.Book.AddAsync(Book);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
+        }
+
     }
 }
