43fd73c48f24fe5ca5e64273d5722159da1330e1
BenAlaa <46384841+BenAlaa@users.noreply.github.com>
2019-12-07 19:41:15 +0200

    Combine both of creat and update in one single page called Upsert

---
+++ XBooks/Pages/BookList/Upsert.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using XBooks.Models;
+
+namespace XBooks
+{
+    public class UpsertModel : PageModel
+    {
+        private readonly ApplicationDBContext _db;
+        public Book Book { get; set; }
+        public UpsertModel(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+
+
+        public async Task<IActionResult> OnGet(int? id)
+        {
+            this.Book = new Book();
+            if (id == null)
+            {
+                // Create
+                return Page();
+            }
+
+            // Update
+            this.Book = await _db.Book.FirstOrDefaultAsync(b => b.Id == id);
+            if (this.Book == null)
+            {
+                return NotFound();
+            }
+            return Page();
+            
+            this.Book = await _db.Book.FindAsync(id);
+        }
+
+        public async Task<IActionResult> OnPost(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                if (book.Id == 0)
+                {
+                    _db.Book.Add(book);
+                }
+                else
+                {
+                    _db.Book.Update(book);
+                }
+
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return RedirectToPage();
+        }
+    }
+}
--- XBooks/wwwroot/js/bookList.js
+++ XBooks/wwwroot/js/bookList.js
@@ -20,7 +20,7 @@ function loadDataTable() {
                 "render": function (data) {
                     return `
                             <div class="row  p-2">
-                                <a href="/BookList/Edit?id=${data}" class="btn btn-primary text-white col-3 offset-lg-3" style="cursor:pointer; ">Edit</a>
+                                <a href="/BookList/Upsert?id=${data}" class="btn btn-primary text-white col-3 offset-lg-3" style="cursor:pointer; ">Edit</a>
                                 <a  class="btn btn-danger text-white col-3 offset-lg-1" style="cursor:pointer; " onclick=Delete('/api/book?id='+${data})>Delete</a>
                             </div>
                     `;
@@ -29,7 +29,7 @@ function loadDataTable() {
             }
 
         ],
-        "language": {
+        "language": { 
             "emptyTable": "No Data Found"
         },
         "width":"100%"
